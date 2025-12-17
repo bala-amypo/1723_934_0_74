6e96e6c7b2bb0e2b4d235d5c534b9e427b6f2db7
bala-amypo <bala@amypo.in>
2025-12-17 06:20:34 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 @Entity
 public class Student {
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
    
     private Long id;
     private String name;
